0f8b709dd669c6f5dff4a31e04ff1cb7cb59932a
nmfzone <nabilftd@gmail.com>
2016-04-04 23:42:42 +0700

    Improve competition terms and condition explanation

--- resources/views/competitions/terms.blade.php
+++ resources/views/competitions/terms.blade.php
@@ -11,10 +11,19 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Terms and Conditions</div>
                     <div class="panel-body content">
-                        Setiap orang yang melakukan pendaftaran Porsematif 2016 melalui website ini, wajib menyetujui dan mematuhi semua hal dibawah ini :<br>
-                        <ul>
-                            <li>Pertama</li>
-                        </ul>
+                        Setiap orang yang melakukan pendaftaran Porsematif 2016, wajib menyetujui dan mematuhi semua hal dibawah ini :<br>
+<h3>Tata Tertib Peserta Lomba</h3>
+<ol>
+    <li>Wajib berpakaian sopan, santun, dan menutup aurat selama acara berlangsung</li>
+    <li>Dilarang menghina yang mengandung hukum SARA (Suku, Agama, Ras, dan Adat istiadat).</li>
+    <li>Dilarang menggangu ketertiban umum, membuat keributan, dan tidak menjaga sopan santun baik secara individu maupun kelompok.</li>
+    <li>Dilarang melakukan tindakan asusila dan kekerasan selama pelaksanaan keegiatan PORSEMATIF 2016 berlangsung.</li>
+    <li>Dilarang membawa minuman keras NAPZA dan benda tajam saat kegiatan berlangsung.</li>
+    <li>Dilarang memakai dan menggunakan minuman keras dan NAPZA selama kegiatan PORSEMATIF 2016 berlangsung.</li>
+</ol><h3>Bentuk Sanksi</h3>
+<b>Ringan</b> : Teguran dan peringatan sebanyak 2 kali oleh divisi keamanan. Jika melanggar aturan 2 dan 3.
+<b>Sedang</b> : Apabila sanksi ringan telah diupayakan tetapi pelanggaran tetap dijalankan      atau melanggar aturan nomor 5 dan 6 maka akan    dikeluarkan dari area kegiatan PORSEMATIF 2016. Dilakukan penyitaan barang-barang terlarang oleh   divisi keamanan.
+<b>Berat</b> : Di laporkan ke pihak berwajib (polisi) jika melanggar peraturan nomor 3 atau mengancam peserta lain dengan tindak kekerasan.
                     </div>
                 </div>
             </div>
