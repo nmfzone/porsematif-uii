3cbadf0730b8ec8978c351963c48da596563d008
nmfzone <nabilftd@gmail.com>
2016-04-04 09:17:40 +0700

    Change email verification display

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -1,11 +1,11 @@
 <center><img src="{{ asset('assets/images/porsematif-big.png') }}" width="60%"></center>
-
-Selamat anda telah bergabung di acara PORSEMATIF 2016, terima kasih atas partisipasi anda.
-
-Silahkan klik tautan di bawah ini untuk mengkonfirmasi akun anda :
-<a href="{{ $link }}">{{ $link }}</a>
-
-Untuk informasi lebih lanjut dan berita terbaru dapat dilihat di akun resmi kami :
+<br>
+Selamat anda telah bergabung di acara PORSEMATIF 2016, terima kasih atas partisipasi anda.<br>
+<br>
+Silahkan klik tautan di bawah ini untuk mengkonfirmasi akun anda :<br>
+<a href="{{ $link }}">{{ $link }}</a><br>
+<br><br>
+Untuk informasi lebih lanjut dan berita terbaru dapat dilihat di akun resmi kami :<br>
 <table border=0>
 <tr>
 <td>Ask.fm</td><td> : </td><td>@Porsematif</td>
